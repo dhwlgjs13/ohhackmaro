406848d556ccae9de033143fd616841ce6865845
maroro0220 <maro1339@gmail.com>
2020-04-15 15:06:01 +0900

    calcul

---
+++ calc.py
@@ -0,0 +1,17 @@
+def minus(a, b):
+    res=int
+
+    return res
+def plus(a,b):
+    res=int
+
+    return res
+def multi(a,b):
+    res=int
+
+    return res
+
+a,b=map(int, input().split())
+minus(a,b)
+plus(a,b)
+multi(a,b)
