fae688a10714ec8cb8417e096c269a057522aa36
maroro0220 <maro1339@gmail.com>
2020-04-15 16:19:38 +0900

    add print

--- calc2.py
+++ calc2.py
@@ -4,7 +4,8 @@ import M_minus
 
 
 if __name__=='__main__':
+    print("enter num1, num2")
     a,b=map(int, input().split())
-    H_multi.minus(a,b)
-    J_plus.plus(a,b)
-    M_minus.multi(a,b)
+    print("H_multi: ",H_multi.multi(a,b))
+    print("J_Plus: ",J_plus.plus(a,b))
+    print("M_minus: ",M_minus.minus(a,b))
