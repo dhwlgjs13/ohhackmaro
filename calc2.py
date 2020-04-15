7ba8ddd9d0bdac1b9353af6d2e0595fceebe1315
maroro0220 <maro1339@gmail.com>
2020-04-15 16:24:50 +0900

    delete init main

--- calc2.py
+++ calc2.py
@@ -3,9 +3,8 @@ import H_multi
 import M_minus
 
 
-if __name__=='__main__':
-    print("enter num1, num2")
-    a,b=map(int, input().split())
-    print("H_multi: ",H_multi.multi(a,b))
-    print("J_Plus: ",J_plus.plus(a,b))
-    print("M_minus: ",M_minus.minus(a,b))
+print("enter num1, num2")
+a,b=map(int, input().split())
+print("H_multi: ",H_multi.multi(a,b))
+print("J_Plus: ",J_plus.plus(a,b))
+print("M_minus: ",M_minus.minus(a,b))
